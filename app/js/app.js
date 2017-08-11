0f032c43417d807955fb4445332d6abfa7a43260
uxicode <jbc2119@naver.com>
2017-08-11 19:10:53 +0900

    basic steps

---
+++ app/js/app.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component{
+    constructor() {
+        super(...arguments);
+        this.state={
+            route:window.location.hash.substr(1)
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('hashchange', ()=>{
+            this.setState({
+                route:window.location.hash.substr(1)
+            });
+        });
+    }
+    render(){
+         return(
+             <div>
+                 <h1>Hello React Router</h1>
+             </div>
+         )
+    }
+}
+ReactDOM.render(<App />, document.getElementById('root'));
